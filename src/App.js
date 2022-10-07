eec5203e8087ccb72155bdf8a53a5a54d03dc3bb
Grepsoft <grepsoft.dev@gmail.com>
2022-10-07 12:17:26 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box, CssBaseline } from "@mui/material";
+import AppState from "./state/appState";
+import appTheme, {palette} from "./styles/theme";
+import AppDrawer from "./components/appDrawer";
+import PrimaryAppBar from "./components/primaryAppbar";
+import "./App.css";
+import Main from "./components/main";
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const colorMode = useMemo(
+      () => ({
+          toggleMode: () => {
+              setMode( prevMode => prevMode === 'light' ? 'dark' : 'light')
+          }
+      }), []
+  );
+
+  const theme = useMemo(
+    () => createTheme({
+      palette:
+      {...palette, mode: mode},
+      ...appTheme
+    })
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppState>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <PrimaryAppBar switchColorMode={colorMode.toggleMode}/>
+          <CssBaseline />
+          <AppDrawer />
+          <Main />
+        </Box>
+      </AppState>
+    </ThemeProvider>
   );
 }
 
